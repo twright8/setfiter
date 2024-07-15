ac53d34f55f8aeb408e45150f015c648a6b5ac61
twright8 <42542027+twright8@users.noreply.github.com>
2024-07-15 22:15:04 +0100

    Update integrations.py

--- src/setfit/integrations.py
+++ src/setfit/integrations.py
@@ -16,15 +16,21 @@ def default_hp_search_backend():
     if is_optuna_available():
         return "optuna"
 
+def run_hp_search_optuna(trainer, n_trials, direction, **kwargs):
 
-def run_hp_search_optuna(trainer: "Trainer", n_trials: int, direction: str, **kwargs) -> BestRun:
-    import optuna
-
-    # Heavily inspired by transformers.integrations.run_hp_search_optuna
-    # https://github.com/huggingface/transformers/blob/cbb8a37929c3860210f95c9ec99b8b84b8cf57a1/src/transformers/integrations.py#L160
-    def _objective(trial):
+    def _objective(trial, checkpoint_dir=None):
+        checkpoint = None
+        if checkpoint_dir:
+            for subdir in os.listdir(checkpoint_dir):
+                if subdir.startswith(PREFIX_CHECKPOINT_DIR):
+                    checkpoint = os.path.join(checkpoint_dir, subdir)
+        if not checkpoint:
+            # free GPU memory
+            del trainer.model
+            gc.collect()
+            torch.cuda.empty_cache()
         trainer.objective = None
-        trainer.train(trial=trial)
+        trainer.train(resume_from_checkpoint=checkpoint, trial=trial)
         # If there hasn't been any evaluation during the training loop.
         if getattr(trainer, "objective", None) is None:
             metrics = trainer.evaluate()
@@ -34,6 +40,7 @@ def run_hp_search_optuna(trainer: "Trainer", n_trials: int, direction: str, **kw
     timeout = kwargs.pop("timeout", None)
     n_jobs = kwargs.pop("n_jobs", 1)
     study = optuna.create_study(direction=direction, **kwargs)
-    study.optimize(_objective, n_trials=n_trials, timeout=timeout, n_jobs=n_jobs)
+    study.optimize(_objective, n_trials=n_trials,
+                   timeout=timeout, n_jobs=n_jobs)
     best_trial = study.best_trial
-    return BestRun(str(best_trial.number), best_trial.value, best_trial.params, study)
+    return BestRun(str(best_trial.number), best_trial.value, best_trial.params)
